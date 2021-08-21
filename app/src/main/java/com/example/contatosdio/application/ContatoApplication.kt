882d69aab47c551cec636535fa9fa97fbaaea525
lWillian <willian.vlcs@gmail.com>
2021-08-20 22:29:39 -0300

    Implementando Helper

--- app/src/main/java/com/example/contatosdio/application/ContatoApplication.kt
+++ app/src/main/java/com/example/contatosdio/application/ContatoApplication.kt
@@ -1,10 +1,15 @@
 package com.example.contatosdio.application
 
 import android.app.Application
+import com.example.contatosdio.helpers.HelperDB
 
 class ContatoApplication : Application(){
+
+    var helperDB: HelperDB? = null
+        private set
+
     override fun onCreate() {
         super.onCreate()
-
+        helperDB = HelperDB(this)
     }
 }
